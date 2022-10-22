9ad4f9a70c8208d9775e4adeb0c04c7e662f3cfa
Sai Wentum <michael.wentum@gmail.com>
2022-10-22 06:57:42 -0400

    Update index.ts
    testing new message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('starting up...');
+  console.log('starting up... and up');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY is not defined');
   }
